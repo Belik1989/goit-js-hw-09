c0b960dee669fdc240c9e6b5898872d50d2309d2
Bilogolovyi Sergii <sergey.hotchili@gmail.com>
2022-12-04 00:41:49 +0800

    Checked

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,22 @@
 
-const body = document.querySelector("body");
+const bodyStyle = document.querySelector("body");
 const startSwitch = document.querySelector('[data-start]');
 const stopSwitch = document.querySelector('[data-stop]');
 let timer = null;
 // Додаємо слухача на кнопки старт та стоп
-startSwitch.addEventListener("click", startRandomizer);
+startSwitch.addEventListener('click', startRandomizer);
 
-stopSwitch.addEventListener("click", stopRandomizer );
+stopSwitch.addEventListener('click', stopRandomizer );
 
-console.log(bodyEll.style.backgroundColor);
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+console.log(bodyStyle.style.backgroundColor);
 function startRandomizer() {
     startSwitch.setAttribute('disabled', 'true');
     stopSwitch.removeAttribute('disabled');
     timer = setInterval(() => {
-    body.style.backgroundColor = getRandomHexColor();
+    bodyStyle.style.backgroundColor = getRandomHexColor();
     }, 350);
 }
 function stopRandomizer() {
@@ -21,6 +24,3 @@ function stopRandomizer() {
     stopSwitch.setAttribute('disabled', 'true');
     clearInterval(timer);
 }
-    function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    }
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -12,7 +12,7 @@ const dataHours = document.querySelector(`.timer [data-hours]`);
 const dataMinutes = document.querySelector(`.timer [data-minutes]`);
 const dataSeconds = document.querySelector(`.timer [data-seconds]`);
 startBtn.classList.add("startBtn");
-const currentDate = Date.now();
+const currentDate = new Date();
 let timeToEnd = {};
 let msTimeToEnd = null;
 
@@ -22,49 +22,42 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        onInputDate(selectedDates[0]);
-    },
-    };
-    
-    const fp = flatpickr('#datetime-picker', options);
-    startBtn.setAttribute('disabled', 'disabled');
-    let timeId = null;
-    const INTERVAL = 1000;
-    
-    function onInputDate(selectedDates) {
-    if (selectedDates <= Date.now()) {
+    console.log(selectedDates[0]);
+    msTimeToEnd = selectedDates[0] - currentDate;
+
+    if (selectedDates[0].getTime() < currentDate.getTime()) {
         Notiflix.Notify.failure('Please choose a date in the future');
+        startBtn.setAttribute('disabled', 'true');
     } else {
-        startBtn.removeAttribute('disabled', 'disabled');
-    onStartedTimer(selectedDates);
+        startBtn.removeAttribute('disabled');
     }
-    }
-    function onStartedTimer(selectedDates) {
-        let msTimeToEnd = Date.parse(selectedDates) - Date.now();
-        let timeToEnd = convertMs(mstimeToEnd);
-        startBtn.addEventListener('click', () => {
-        startBtn.setAttribute('disabled', 'disabled');
-        dataTimePicker.setAttribute('disabled', 'disabled');
-        timeId = setInterval(() => {
+    },
+};
+
+flatpickr(dataTimePicker, options);
+startBtn.addEventListener('click', startHandler);
+startBtn.setAttribute('disabled', 'true');
+
+function startHandler() {
+    setInterval(() => {
     if (msTimeToEnd <= 0) {
-        clearInterval(timeId);
-    return;
+        return;
     }
     timeToEnd = convertMs(msTimeToEnd);
-    dataDays.textContent = timeToEnd.days.toString().padStart(2, '0')
-    dataHours.textContent = timeToEnd.hours.toString().padStart(2, '0')
-    dataMinutes.textContent = timeToEnd.minutes.toString().padStart(2, '0')
-    dataSeconds.textContent = timeToEnd.seconds.toString().padStart(2, '0')
-    msTimeToEnd -= INTERVAL;
-    },INTERVAL);
-});
-
-}function convertMs(ms) {
+    dataDays.textContent = timeToEnd.days.toString().padStart(2, '0');
+    dataHours.textContent = timeToEnd.hours.toString().padStart(2, '0');
+    dataMinutes.textContent = timeToEnd.minutes.toString().padStart(2, '0');
+    dataSeconds.textContent = timeToEnd.seconds.toString().padStart(2, '0');
+    msTimeToEnd -= 1000;
+    }, 1000);
+}
+function convertMs(ms) {
     // Number of milliseconds per unit of time
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
+
     // Remaining days
     const days = Math.floor(ms / day);
     // Remaining hours
@@ -73,7 +66,5 @@ const options = {
     const minutes = Math.floor(((ms % day) % hour) / minute);
     // Remaining seconds
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
     return { days, hours, minutes, seconds };
-    
-    }
+}
