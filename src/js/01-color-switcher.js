c211739822348f01f4038c2e326fb47be1fca898
Bilogolovyi Sergii <sergey.hotchili@gmail.com>
2022-12-03 23:31:16 +0800

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,31 +2,25 @@
 const body = document.querySelector("body");
 const startSwitch = document.querySelector('[data-start]');
 const stopSwitch = document.querySelector('[data-stop]');
-
-// Додаємо слухача на кнопки старт та стопs
+let timer = null;
+// Додаємо слухача на кнопки старт та стоп
 startSwitch.addEventListener("click", startRandomizer);
 
 stopSwitch.addEventListener("click", stopRandomizer );
 
-function startRandomizer(event){
-    timerId = setInterval(() => {
-        const color = getRandomHexColor();
-        body.style.backgroundColor = color;
-        console.log(getRandomHexColor());
-    }, 500);
-    event.target.disabled = true;
-    document.querySelector('[data-stop]').disabled = false;
-    console.log(`Start RANDOMIZER`);
+console.log(bodyEll.style.backgroundColor);
+function startRandomizer() {
+    startSwitch.setAttribute('disabled', 'true');
+    stopSwitch.removeAttribute('disabled');
+    timer = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    }, 350);
 }
-
-function stopRandomizer(event){
-    clearInterval(timerId);
-    event.target.disabled = true;
-    document.querySelector('[data-start]').disabled = false;
-console.log(`Stop RANDOMIZER`);
-
-} 
-
-function getRandomHexColor() {
+function stopRandomizer() {
+    startSwitch.removeAttribute('disabled');
+    stopSwitch.setAttribute('disabled', 'true');
+    clearInterval(timer);
+}
+    function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+    }
